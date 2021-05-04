024b8632b51f30999ebc0aa73312df48f6c3360c
Alexander <79066982+godlixe@users.noreply.github.com>
2021-05-04 23:12:32 +0700

    Update Trackbar dan Bounding Box.cpp

--- Trackbar dan Bounding Box.cpp
+++ Trackbar dan Bounding Box.cpp
@@ -14,17 +14,18 @@ Scalar greenHigh = Scalar(70, 255, 255);
 int main()
 {
 
-    Mat frame, fullImageHSV,frame_threshold, mask;
+    Mat frame, fullImageHSV, fullImageHSV2, frame_threshold, frame_threshold2, mask;
     VideoCapture cap(-1);
-    namedWindow("My Window", 1);
+    namedWindow("My Window", WINDOW_NORMAL);
+    setWindowProperty("My Window", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
 
-    int lh=0, ls=0, lv=0, uh=180, us=255, uv=255;
-    createTrackbar("LH", "My Window", &lh, uh);
-    createTrackbar("LS", "My Window", &ls, us);
-    createTrackbar("LV", "My Window", &lv, uv);
-    createTrackbar("UH", "My Window", &uh, uh);
-    createTrackbar("US", "My Window", &us, us);
-    createTrackbar("UV", "My Window", &uv, uv);
+    int glh=0, gls=0, glv=0, rlh=0, rls=0, rlv=0, uh=180, us=255, uv=255;
+    createTrackbar("Green_LH", "My Window", &glh, uh);
+    createTrackbar("Green_LS", "My Window", &gls, us);
+    createTrackbar("Green_LV", "My Window", &glv, uv);
+    createTrackbar("Red_LH", "My Window", &rlh, uh);
+    createTrackbar("Red_LS", "My Window", &rls, us);
+    createTrackbar("Red_LV", "My Window", &rlv, uv);
     
     if(!cap.open(0))
         return 0;
@@ -32,15 +33,33 @@ int main()
     {
             cap >> frame;
             cvtColor(frame, fullImageHSV, COLOR_BGR2HSV);
+            cvtColor(frame, fullImageHSV2, COLOR_BGR2HSV);
             if( frame.empty() ) break;
-            //inRange(fullImageHSV, Scalar(lh, ls, lv), Scalar(uh, us, uv), frame_threshold);
+            inRange(fullImageHSV, Scalar(glh, gls, glv), Scalar(uh, us, uv), frame_threshold);
+            inRange(fullImageHSV2, Scalar(rlh, rls, rlv), Scalar(uh, us, uv), frame_threshold2);
             //cvtColor(frame, frame, COLOR_BGR2HSV);
-            inRange(frame, greenLow, greenHigh, mask);
+            //inRange(frame, greenLow, greenHigh, mask);
+            
             vector <vector<Point>> contours;
-            findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-            drawContours(frame, contours, 1, (0,255,0), 3);
+            findContours(frame_threshold, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+            //drawContours(frame_threshold, contours, 1, (0,255,0), 3);
+            for (size_t i = 0; i < contours.size(); i++){
+                Rect boundRect = boundingRect(contours[i]);
+                if(boundRect.area()>350 && (boundRect.width < 70|| boundRect.height<70)){
+                    rectangle(frame, boundRect.tl(), boundRect.br(), (0,0,255), 2);
+                }
+            }
+            vector <vector<Point>> contours2;
+            findContours(frame_threshold2, contours2, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+            //drawContours(frame_threshold, contours, 1, (0,255,0), 3);
+            for (size_t i = 0; i < contours2.size(); i++){
+                Rect boundRect = boundingRect(contours2[i]);
+                if(boundRect.area()>350 && (boundRect.width < 70|| boundRect.height<70)){
+                    rectangle(frame, boundRect.tl(), boundRect.br(), (255,0,0), 2);
+                }
+            }
+            //imshow("My Window", frame);
             imshow("My Window", frame);
-            //imshow("My Window", frame_threshold);
             if( waitKey(10) >=0 ) break;
     }
     return 0;
